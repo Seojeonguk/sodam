9af9e409d098cbb65475d604f5878e7b322afe2c
Seojeonguk <junguk7880@naver.com>
2025-06-06 20:49:56 +0900

    feat: eslint 초기 설정 (#18)
    공통 설정 eslint를 초기 설정 후 적용합니다.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,28 +1,89 @@
-import js from "@eslint/js";
 import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import pluginReactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ...pluginJs.configs.recommended,
+  },
+  {
     files: ["**/*.{ts,tsx}"],
+    extends: [
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ["./tsconfig.eslint.json"],
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
     },
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      react: pluginReact,
+      "react-hooks": pluginReactHooks,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+      ...pluginReactHooks.configs.recommended.rules,
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    plugins: {
+      "react-refresh": pluginReactRefresh,
+    },
+    rules: {
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
     },
+  },
+  {
+    ...prettierConfig,
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/ban-ts-comment": "warn",
+    },
+  },
+  {
+    ignores: [
+      "dist/",
+      "node_modules/",
+      "**/*.jsconfig.json",
+      "eslint.config.js",
+      ".prettierrc.cjs",
+    ],
   },
 );
