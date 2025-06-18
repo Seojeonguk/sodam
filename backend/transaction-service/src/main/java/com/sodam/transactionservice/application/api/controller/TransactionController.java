a4a90d8e0e1d8939194554e0002e56f8e8448194
Seojeonguk <junguk7880@naver.com>
2025-06-19 01:07:16 +0900

    feat: 거래 서비스 origins 설정
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- backend/transaction-service/src/main/java/com/sodam/transactionservice/application/api/controller/TransactionController.java
+++ backend/transaction-service/src/main/java/com/sodam/transactionservice/application/api/controller/TransactionController.java
@@ -14,6 +14,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = {
+        "http://localhost:5173",
+        "https://sodam-nine.vercel.app",
+        "http://127.0.0.1:5173"
+})
 @RestController
 @RequestMapping("/api/transactions")
 @RequiredArgsConstructor
