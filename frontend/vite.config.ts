d4613b66448f1742628d1098310cc0a87380ada5
Seojeonguk <junguk7880@naver.com>
2025-06-19 00:07:07 +0900

    chore: vite 개발 서버 proxy 설정 수정
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,24 @@
-import { defineConfig, loadEnv  } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {plugins: [react()],
+  return {
+    plugins: [react()],
     server: {
-    proxy: {
-      '/api/transactions': { // /api로 시작하는 모든 요청을 프록시
-        target: `${env.VITE_API_TARGET_URL}:${env.VITE_API_TRANSACTION_SERVICE_PORT}` || 'http://localhost:10001',
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, ''), // /api를 제거하고 백엔드로 전달
+      proxy: {
+        "/api": {
+          // /api로 시작하는 모든 요청을 프록시
+          target:
+            env.VITE_API_TARGET_URL && env.VITE_API_TRANSACTION_SERVICE_PORT
+              ? `${env.VITE_API_TARGET_URL}:${env.VITE_API_TRANSACTION_SERVICE_PORT}`
+              : "http://localhost:10001",
+          changeOrigin: true,
+          // rewrite: (path) => path.replace(/^\/api/, ''), // /api를 제거하고 백엔드로 전달
+        },
       },
     },
-  },
-}
+  };
 });
