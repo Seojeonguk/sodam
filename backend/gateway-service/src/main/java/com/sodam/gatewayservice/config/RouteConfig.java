949ee6bfbaf8445dfd789d40bc8806b91a12e490
Seojeonguk <junguk7880@naver.com>
2025-08-16 02:19:16 +0900

    feat: 라우팅 설정 추가
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ backend/gateway-service/src/main/java/com/sodam/gatewayservice/config/RouteConfig.java
@@ -0,0 +1,20 @@
+package com.sodam.gatewayservice.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("user-service", r -> r.path("/api/auth/**")
+                        .uri("lb://user-service"))
+                .route("transaction-service", r -> r.path("/api/transactions/**")
+                        .uri("lb://transaction-service"))
+                .build();
+    }
+}
