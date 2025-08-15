285ab50a0f540119369244143f210916e22009d5
Seojeonguk <junguk7880@naver.com>
2025-08-16 03:23:19 +0900

    feat: 사용자 API 일부 필터 적용
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- backend/gateway-service/src/main/java/com/sodam/gatewayservice/config/RouteConfig.java
+++ backend/gateway-service/src/main/java/com/sodam/gatewayservice/config/RouteConfig.java
@@ -1,5 +1,6 @@
 package com.sodam.gatewayservice.config;
 
+import com.sodam.gatewayservice.filter.JwtAuthGatewayFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +12,10 @@ public class RouteConfig {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("user-service", r -> r.path("/api/auth/**")
+                .route("user-service-no-filter", r -> r.path("/api/auth/login", "/api/auth/logout", "/api/auth/register")
+                        .uri("lb://user-service"))
+                .route("user-service-with-filter", r -> r.path("/api/auth/**")
+                        .filters(f -> f.filter(new JwtAuthGatewayFilter()))
                         .uri("lb://user-service"))
                 .route("transaction-service", r -> r.path("/api/transactions/**")
                         .uri("lb://transaction-service"))
