e1d0c130e258885bedd1a06ead3d0bdbfe8cf90d
Seojeonguk <junguk7880@naver.com>
2025-09-06 21:05:17 +0900

    feat: 카테고리 엔티티 생성
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ backend/category-service/src/main/java/com/example/categoryservice/domain/model/Category.java
@@ -0,0 +1,37 @@
+package com.example.categoryservice.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description", length = 255)
+    private String description;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "color")
+    private String color;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
