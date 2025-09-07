1c68c602461744f825743870b08274886963343e
Seojeonguk <junguk7880@naver.com>
2025-09-07 21:49:33 +0900

    feat: FeignClients 설정 어노테이션 추가
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- backend/category-service/src/main/java/com/example/categoryservice/CategoryServiceApplication.java
+++ backend/category-service/src/main/java/com/example/categoryservice/CategoryServiceApplication.java
@@ -2,8 +2,12 @@ package com.example.categoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
+@EnableFeignClients
 public class CategoryServiceApplication {
 
     public static void main(String[] args) {
