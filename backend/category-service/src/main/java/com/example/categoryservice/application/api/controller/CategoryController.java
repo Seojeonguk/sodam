897e4575f81cdb25e0bbaa17456d9807dd2a0099
Seojeonguk <junguk7880@naver.com>
2025-09-07 23:23:22 +0900

    feat: 카테고리 목록 조회 API 추가
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- backend/category-service/src/main/java/com/example/categoryservice/application/api/controller/CategoryController.java
+++ backend/category-service/src/main/java/com/example/categoryservice/application/api/controller/CategoryController.java
@@ -1,9 +1,12 @@
 package com.example.categoryservice.application.api.controller;
 
 import com.example.categoryservice.application.api.dto.CategoryCreateRequest;
+import com.example.categoryservice.application.api.dto.CategoryListRequest;
+import com.example.categoryservice.application.api.dto.CategoryListResponse;
 import com.example.categoryservice.application.api.dto.CategoryResponse;
 import com.example.categoryservice.application.service.CategoryApplicationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,5 +20,14 @@ public class CategoryController {
     @PostMapping()
     public ResponseEntity<CategoryResponse> createCategory(@RequestBody CategoryCreateRequest category, @RequestHeader("X-User-Email") String email) {
         return ResponseEntity.ok(service.createCategory(category, email));
+    }
+
+    @GetMapping
+    public ResponseEntity<CategoryListResponse> getCategories(
+            @ModelAttribute CategoryListRequest request,
+            Pageable pageable,
+            @RequestHeader("X-User-Email") String email
+    ) {
+        return ResponseEntity.ok(service.getCategories(request, pageable, email));
     }
 }
