efeee648162ee1867fd23168d2bbb0b171b4095c
Seojeonguk <junguk7880@naver.com>
2025-09-14 18:55:11 +0900

    feat: 가계부 서비스 초기 설정
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ backend/account-book-service/src/main/java/com/sodam/accountbookservice/AccountBookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sodam.accountbookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountBookServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountBookServiceApplication.class, args);
+    }
+
+}
