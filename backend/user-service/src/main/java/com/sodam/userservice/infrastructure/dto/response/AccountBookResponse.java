0416b6dfbf1f499d621dedc9b1217a2f0316985e
Seojeonguk <junguk7880@naver.com>
2026-02-20 23:51:03 +0900

    fix: 자료형 수정에 따른 response 자료형 수정
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- backend/user-service/src/main/java/com/sodam/userservice/infrastructure/dto/response/AccountBookResponse.java
+++ backend/user-service/src/main/java/com/sodam/userservice/infrastructure/dto/response/AccountBookResponse.java
@@ -2,10 +2,8 @@ package com.sodam.userservice.infrastructure.dto.response;
 
 import lombok.Data;
 
-import java.time.LocalDateTime;
-
 @Data
 public class AccountBookResponse {
     private String name;
-    private LocalDateTime updatedAt;
+    private String updatedAt;
 }
