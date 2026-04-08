cd52bf86c0c12bc8b1ae09a9431bff15ae397f4f
Seojeonguk <junguk7880@naver.com>
2026-04-08 11:35:39 +0900

    feat: 공통 exception 설정
    공통 모듈에서 exception을 정의하고 처리합니다.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

---
+++ backend/common-module/src/main/java/com/sodam/common/exception/CustomException.java
@@ -0,0 +1,15 @@
+package com.sodam.common.exception;
+
+import com.sodam.common.response.ResponseCode;
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+    private final ResponseCode responseCode;
+
+    public CustomException(ResponseCode responseCode) {
+        super(responseCode.getMessage());
+        this.responseCode = responseCode;
+    }
+
+}
--- backend/common-module/src/main/java/com/sodam/common/exception/GlobalExceptionHandler.java
+++ backend/common-module/src/main/java/com/sodam/common/exception/GlobalExceptionHandler.java
@@ -2,19 +2,38 @@ package com.sodam.common.exception;
 
 import com.sodam.common.response.ApiResponse;
 import com.sodam.common.response.ResponseCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ApiResponse<Void>> handleCustomException(CustomException ex) {
+        ResponseCode code = ex.getResponseCode();
+
+        return ResponseEntity
+                .status(code.getStatus())
+                .body(ApiResponse.fail(code.getCode(), code.getMessage()));
+    }
+
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ApiResponse<Void>> handleIllegalArgumentException(IllegalArgumentException ex) {
-        return ResponseEntity.internalServerError().body(ApiResponse.fail(ResponseCode.BAD_REQUEST.getCode(), ex.getMessage()));
+        return ResponseEntity
+                .badRequest()
+                .body(ApiResponse.fail(ResponseCode.BAD_REQUEST.getCode(), ResponseCode.BAD_REQUEST.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleException(Exception ex) {
-        return ResponseEntity.internalServerError().body(ApiResponse.fail(ResponseCode.INTERNAL_SERVER_ERROR.getCode(), ex.getMessage()));
+        log.error("Unhandled Exception", ex);
+
+        return ResponseEntity
+                .internalServerError()
+                .body(ApiResponse.fail(ResponseCode.INTERNAL_SERVER_ERROR.getCode(), ResponseCode.INTERNAL_SERVER_ERROR.getMessage()));
     }
+
 }
